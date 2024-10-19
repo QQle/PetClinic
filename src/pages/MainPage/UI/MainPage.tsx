bac7742813c849a6334ac0057b9623caa6cbaf48
b.valiullin <b.valiullin@solber.ru>
2024-10-19 13:21:47 +0300

    add topTitle

--- src/pages/MainPage/UI/MainPage.tsx
+++ src/pages/MainPage/UI/MainPage.tsx
@@ -7,15 +7,32 @@ import { cardInfoPers } from "shared/mock data/cardInfo";
 const MainPage = () => {
   return (
     <div className={cls.MainPage}>
-      <section className={cls.top}></section>
+      <section className={cls.top}>
+        <div className={cls.top_box}>
+          <h1>
+            Pet <br></br> Clinic
+          </h1>
+          <div className={cls.top_box_info}>
+            <span>Круглосуточная ветеринарная клиника</span>
+            <p>+7 999 430 74 32</p>
+            <div>
+              <a href="https://go.2gis.com/o8gnw" target="_blank">
+                Москва, м. Алексеевская, Зубарев переулок д. 7
+              </a>
+              <img src="https://vet.city/upload/iblock/dde/p2s5n0fk7eh6vgg7weipdz7f7cjpdnn0.svg" />
+            </div>
+          </div>
+        </div>
+      </section>
+
       <section className={cls.infoCards}>
         <div className={cls.infoCardPers}>
-          <span>врачи vetcity clinic</span>
+          <span>врачи PetClinic</span>
           <h2>{cardInfoPers.title}</h2>
           <p>{cardInfoPers.description}</p>
           <span>{cardInfoPers.desr}:</span>
           {cardInfoPers.group.map((item) => (
-            <ul>{item}</ul>
+            <li>{item}</li>
           ))}
         </div>
         <div className={cls.infoCard}>
